dd68f2096ad458cdb079e5a0ac83dcfc60bcccb3
Ember Kerstetter <emberk@udel.edu>
2025-03-03 13:28:53 -0500

    Coded sumPoints, sumPublishedPoints, toCSV

--- src/nested.ts
+++ src/nested.ts
@@ -46,7 +46,9 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    return questions.filter(
+        (question: Question): boolean => question.id !== id,
+    );
 }
 
 /***
@@ -54,21 +56,29 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    return questions.map((question: Question): string => question.name);
 }
 
 /***
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce(
+        (currentScore: number, question: Question): number =>
+            currentScore + question.points,
+        0,
+    );
 }
 
 /***
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce(
+        (currentScore: number, question: Question): number =>
+            question.published ? currentScore + question.points : currentScore,
+        0,
+    );
 }
 
 /***
@@ -89,7 +99,23 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    return (
+        "id,name,options,points,published\n" +
+        questions
+            .map(
+                (question: Question): string =>
+                    question.id +
+                    "," +
+                    question.name +
+                    "," +
+                    question.options.length +
+                    "," +
+                    question.points +
+                    "," +
+                    question.published,
+            )
+            .join("\n")
+    );
 }
 
 /**
