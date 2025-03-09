c1bbbef3390774fc43a2d45c6c572d4f9fc09d8b
Ember Kerstetter <emberk@udel.edu>
2025-03-08 21:38:18 -0500

    Coded TwoDice, all relevant tests pass

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
-// import { TwoDice } from "./components/TwoDice";
+import { TwoDice } from "./components/TwoDice";
 // import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
@@ -20,8 +20,8 @@ function App(): JSX.Element {
             <hr />
             <StartAttempt></StartAttempt>
             <hr />
-            {/*<TwoDice></TwoDice>
-            <hr />*/}
+            <TwoDice></TwoDice>
+            <hr />
             <ChangeType></ChangeType>
             {/*<hr />
             <CycleHoliday></CycleHoliday>*/}
